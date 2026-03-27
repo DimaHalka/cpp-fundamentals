b1e858debf8aa4b5ed17daa8e3d404dea7d4dd02
Dmitriy Galka <Dmitriy.Galka@materialise.com>
2026-03-27 12:26:36 +0200

    Missing includes, VS2022

--- factory.h
+++ factory.h
@@ -2,29 +2,30 @@
 
 #include <functional>
 #include <memory>
+#include <string>
 #include <unordered_map>
 
 
 struct factory_creatable {
-    virtual ~factory_creatable() = default;
+  virtual ~factory_creatable() = default;
 };
 
 
 class factory {
 public:
-    using obj_pointer = std::unique_ptr<factory_creatable>;
-    using creator = std::function<obj_pointer()>;
+  using obj_pointer = std::unique_ptr<factory_creatable>;
+  using creator = std::function<obj_pointer()>;
 
-    static factory& instance();
-    
-    void register_type(const std::string& type_id, creator);
-    bool is_registered(const std::string& type_id);
-    void unregister_type(const std::string& type_id);
-    
-    obj_pointer create(const std::string& type_id);
-    
+  static factory& instance();
+
+  void register_type(const std::string& type_id, creator);
+  bool is_registered(const std::string& type_id);
+  void unregister_type(const std::string& type_id);
+
+  obj_pointer create(const std::string& type_id);
+
 private:
-    factory() = default;
-    
-    std::unordered_map<std::string, creator> m_map;
+  factory() = default;
+
+  std::unordered_map<std::string, creator> m_map;
 };
